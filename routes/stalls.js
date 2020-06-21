bd1635757178b017773bda8be4870c8a788c60a5
DucMinh2310 <nickcuaducminh@gmail.com>
2020-06-22 00:25:08 +0700

    third update

--- routes/stalls.js
+++ routes/stalls.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Stall = require("../models/Stall");
+const middleware = require("../middleware/middleware");
 
-router.get("/new", (req, res) => {
+router.get("/new", middleware.isLoggedIn, (req, res) => {
   res.render("stalls/newStall");
 });
 
@@ -12,7 +13,11 @@ router.post("/new", (req, res) => {
   const image = req.body.image;
   const description = req.body.description;
   const price = req.body.price;
-  const newStall = { name, image, description, price };
+  const author = {
+    id: req.user._id,
+    username: req.user.username,
+  };
+  const newStall = { name, image, description, price, author };
   Stall.create(newStall, (err, newlyStall) => {
     if (err) {
       console.log(err);
@@ -39,7 +44,7 @@ router.get("/:id", (req, res) => {
 });
 
 // form edit
-router.get("/:id/edit", (req, res) => {
+router.get("/:id/edit", middleware.isLoggedIn, (req, res) => {
   const id = req.params.id;
   Stall.findById(id, (err, foundStall) => {
     if (err) {
@@ -52,7 +57,7 @@ router.get("/:id/edit", (req, res) => {
   });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", middleware.isLoggedIn, (req, res) => {
   const id = req.params.id;
   const name = req.body.name;
   const image = req.body.image;
@@ -68,7 +73,7 @@ router.put("/:id", (req, res) => {
   });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", middleware.isLoggedIn, (req, res) => {
   Stall.findByIdAndRemove(req.params.id, (err) => {
     if (err) {
       console.log(err);
